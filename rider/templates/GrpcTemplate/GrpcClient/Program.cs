e803e29079605dbaf90b29838ecd145f5bee5b36
AutMai <shuber0802@gmail.com>
2022-02-23 21:56:36 +0100

    added GrpcTemplate

---
+++ rider/templates/GrpcTemplate/GrpcClient/Program.cs
@@ -0,0 +1,10 @@
+﻿using Greeter;
+using Grpc.Net.Client;
+
+var channel = GrpcChannel.ForAddress("http://localhost:5151");
+var client = new Greeter.Greeter.GreeterClient(channel);
+
+var response = client.SayHello(
+    new HelloRequest() { Name = "World" });
+
+Console.WriteLine(response.Message);
